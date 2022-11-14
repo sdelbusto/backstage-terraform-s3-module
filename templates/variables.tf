f614f9b5c879cacc2a32812c0d5394a1238aa962
sdelbusto <sanda.delbusto@gmail.com>
2022-11-14 19:12:37 +0100

    fix: empty variables

--- templates/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "region" {
-  default = "eu-west-1"
-}
-
-variable "bucket_prefix" {
-  type    = string
-  description = "Set bucket prefix without special characters"
-  default = "mybuckets3"
-}
